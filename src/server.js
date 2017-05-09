49ddf0610102e88f41205da83b8152814b212e85
Saikiran Sheshagiri <saikiran.sheshagiri@gmail.com>
2017-05-09 16:20:08 -0500

    updated with the router

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const router = express.Router();
+
+router.get('/', (request, response) => {
+    response.send('Hello World...!!!');
+})
+
+app.use(router);
+
 const server = app.listen(3000, () => {
     const {address, port} = server.address();
     console.log(`Example running on at http://${address}: ${port}`);
