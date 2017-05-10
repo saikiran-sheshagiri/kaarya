f0ce1d11b5ab685da3d7e040d62c91f3054ad911
Saikiran Sheshagiri <saikiran.sheshagiri@gmail.com>
2017-05-10 13:43:41 -0500

    added mongoose and updated database connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import router from './router'
+import mongoose from 'mongoose';
 
 const app = express();
+
+//connect to the mongodb instance
+mongoose.connect('mongodb://localhost:27017/kaarya');
+mongoose.connection.on('error', () => {
+    console.log('Unable to connect to database');
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to local instance of the database');
+});
+
 
 //mount the routes on api
 app.use('/api', router);
